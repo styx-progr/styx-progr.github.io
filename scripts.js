908c63ce648f2db1f44a37a0a60a8002d8a69067
styx-progr <thomas.lingl96@gmail.com>
2022-06-23 19:15:27 +0200

    first script

--- scripts.js
+++ scripts.js
@@ -3,21 +3,22 @@ function draw() {
     const canvas = document.getElementById("canvas");
 
     const resize = () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
     }
 
     resize()
     window.addEventListener('resize', resize)
-    // const canvas = document.getElementById('canvas');
-    // if (canvas.getContext) {
-    // const ctx = canvas.getContext('2d');
 
-    // ctx.fillStyle = 'rgb(200, 0, 0)';
-    // ctx.fillRect(10, 10, 50, 50);
+    if (canvas.getContext) {
+        const ctx = canvas.getContext('2d');
 
-    // ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    // ctx.fillRect(30, 30, 50, 50);
-    // }
+        ctx.fillStyle = 'rgb(200, 0, 0)';
+        ctx.fillRect(10, 10, 20, 20);
+
+        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        ctx.fillRect(20, 20, 30, 30);
+      }
+
 }
 
