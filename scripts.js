f10b971a57a2264c703d8937d1644207328a0d13
styx-progr <thomas.lingl96@gmail.com>
2022-06-24 11:46:44 +0200

    test for getting a square on click

--- scripts.js
+++ scripts.js
@@ -22,3 +22,25 @@ function draw() {
 
 }
 
+
+function  getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect(), // abs. size of element
+      scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for x
+      scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for y
+  
+    return {
+      x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
+      y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+    }
+  }
+
+  function drawSquare(canvas, evt) {
+    x, y = getMousePos(canvas, evt);
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect(x, y, x+10, y+10);
+  }
+
+  let btn = document.getElementById("canvas");
+  btn.addEventListener('click', event => {
+    drawSquare(canvas, event);
+  });
