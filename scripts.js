1cef3dfeac28bcb5ecf097b1b3f196b3ee3b10b2
styx-progr <thomas.lingl96@gmail.com>
2022-06-27 16:04:05 +0200

    update script: print position of mouse

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-
+const canvas = document.getElementById("pixelart");
 function draw() {
     const canvas = document.getElementById("pixelart");
 
@@ -27,7 +27,7 @@ function  getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect(), // abs. size of element
     scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for x
     scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for y
-  
+    alert("Calculating the position")
     return {
       x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
       y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
@@ -46,3 +46,5 @@ function  getMousePos(canvas, evt) {
     ctx.fillRect(x-5, y-5, x+5, y+5);
   }
 
+
+  canvas.addEventListener('click', getMousePos(canvas), false);
