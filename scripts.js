b106d51324f49e18635bf013a1c0a9e28696d312
styx-progr <thomas.lingl96@gmail.com>
2022-06-27 17:50:18 +0200

    updated alert, can draw squares on canvas

--- scripts.js
+++ scripts.js
@@ -1,38 +1,23 @@
-var canvas = document.getElementById("pixelart");
-if(canvas){
-  canvas.addEventListener('click', drawTestSquares(), false);
-}
-
-function drawTestSquares() {
-  var c = document.getElementById("pixelart");
-  var ctx = c.getContext("2d");
-  ctx.fillStyle = "#FF0000";
-  ctx.fillRect(0, 0, 10, 10);
-
-  ctx.fillStyle = "#04B45F";
-  ctx.fillRect(10, 10, 10, 10);
-
-  ctx.fillStyle = "#00FFFF";
-  ctx.fillRect(20, 20, 10, 10);
-
-  ctx.fillStyle = "#C8FE2EF";
-  ctx.fillRect(30, 30, 10, 10);
-  
-  ctx.fillStyle = "#FE2EC8";
-  ctx.fillRect(140, 140, 10, 10);
-
-  ctx.fillStyle = "#FE2EC8";
-  ctx.fillRect(400, 300, 30, 30);
-}
-
-function  getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect(), // abs. size of element
-    scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for x
-    scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for y
-    console.log("Getting positions")
-    return {
-      x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
-      y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+function loadCanvas() {
+  var canvas = document.getElementById("pixelart");
+  var ctx = canvas.getContext("2d");
+  for (let i=0; i<180;i++){
+    for (let j=0; j<90;j++){
+      ctx.fillStyle = "#F2F2F2";
+      ctx.fillRect(i*10, j*10, 10, 10);
     }
   }
+
+}
+
+function drawSquare(canvas, event) {
+  const rect = canvas.getBoundingClientRect()
+  const x = event.clientX - rect.left
+  const y = event.clientY - rect.top
+  console.log("x: " + x + " y: " + y)
+  // var drawing = document.getElementById("pixelart");
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#04B45F";
+  ctx.fillRect(Math.floor(x/10)*10, Math.floor(y/10)*10, 10, 10);
+}
 
