728f83b641eea047feca762b64b155d2190b3ef9
styx-progr <thomas.lingl96@gmail.com>
2022-06-28 16:08:03 +0200

    update pixelart: fixed position of canvas and colour bar

--- scripts.js
+++ scripts.js
@@ -21,3 +21,11 @@ function drawSquare(canvas, event) {
   ctx.fillRect(Math.floor(x/10)*10, Math.floor(y/10)*10, 10, 10);
 }
 
+// function fitToContainer(canvas){
+//   // Make it visually fill the positioned parent
+//   canvas.style.width ='100%';
+//   canvas.style.height='100%';
+//   // ...then set the internal size to match
+//   canvas.width  = canvas.offsetWidth;
+//   canvas.height = canvas.offsetHeight;
+// }
