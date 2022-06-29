b8d76255304c7d968998143de9c3fd5f3d9915b6
styx-progr <thomas.lingl96@gmail.com>
2022-06-29 14:11:18 +0200

    update script file

--- scripts.js
+++ scripts.js
@@ -1,13 +1,35 @@
 function loadCanvas() {
   var canvas = document.getElementById("pixelart");
   var ctx = canvas.getContext("2d");
+  var colourdict = {};
   for (let i=0; i<180;i++){
     for (let j=0; j<90;j++){
+      colourdict = initColorFile(colourdict, i*10, j*10);
       ctx.fillStyle = "#F2F2F2";
       ctx.fillRect(i*10, j*10, 10, 10);
     }
   }
+  // save the colourdict to a file
+  const fs = require('fs');
+  var colourfile = JSON.parse(colourdict);
+  console.log(colourfile);
+  var colourfileContent = JSON.stringify(colourfile);
+  console.log(colourfileContent);
+  // Now we write all colours and their positions to a JSON file
+  fs.writeFile("colours.json", colourfileContent, 'utf8', function(err) {
+    if (err) {
+      console.log("There was an error while writing to JSON file");
+      return console.log(err);
+    }
+    console.log("JSON file has been saved successfully!")
+  })
+}
 
+function initColorFile(colourdict, xpos, ypos) {
+  xstring = xpos.toString();
+  ystring = ypos.toString();
+  colourdict[xstring+ystring] = "#F2F2F2"
+  return colourdict;
 }
 
 function drawSquare(canvas, event) {
